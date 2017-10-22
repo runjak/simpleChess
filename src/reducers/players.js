84326f5127a0a8c181933ed7fe634f67a51550bf
Jakob Runge <sicarius@g4t3.de>
2017-10-22 23:28:33 +0200

    let players reducer handle player actions

--- src/reducers/players.js
+++ src/reducers/players.js
@@ -3,6 +3,7 @@ import type {
   Color,
   Position,
 } from '../utils/Field';
+import type { PlayerActions } from '../actions/player';
 
 export type PlayerState = {
   currentTurn: Color,
@@ -14,7 +15,44 @@ export const initialState = {
   selectedField: null,
 };
 
+function selectFigure(state: PlayerState, position: Position): PlayerState {
+  if (state.selectedField) {
+    return state;
+  }
+
+  return {
+    ...state,
+    selectedField: position,
+  };
+}
+
+function selectTargetField(state: PlayerState): PlayerState {
+  const { currentTurn, selectedField } = state;
+
+  if (!selectedField) {
+    return state;
+  }
+
+  return {
+    ...state,
+    selectedField: null,
+    currentTurn: (currentTurn === 'white') ? 'black' : 'white',
+  };
+}
+
 // eslint-disable-next-line no-unused-vars
-export default function reduce(state: PlayerState = initialState, action: any): PlayerState {
-  return state;
+export default function reduce(
+  state: PlayerState = initialState,
+  action: PlayerActions,
+): PlayerState {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'SELECT_FIGURE':
+      return selectFigure(state, payload);
+    case 'SELECT_TARGET_FIELD':
+      return selectTargetField(state);
+    default:
+      return state;
+  }
 }
--- src/reducers/players.test.js
+++ src/reducers/players.test.js
@@ -4,14 +4,103 @@ describe('players reducer', () => {
   it('should return the given state for an unknown action', () => {
     const state = {};
 
-    const actual = reduce(state, {});
+    const actual = reduce(state, {
+      type: '',
+    });
 
     expect(actual).toBe(state);
   });
 
   it('should return the initialState as state given an undefined state', () => {
-    const actual = reduce();
+    const actual = reduce(undefined, {
+      type: '',
+    });
 
     expect(actual).toEqual(initialState);
+  });
+
+  describe('with SelectFigureAction', () => {
+    it('should select the given field', () => {
+      const expected = {
+        currentTurn: 'white',
+        selectedField: 'foobar',
+      };
+
+      const actual = reduce(undefined, {
+        type: 'SELECT_FIGURE',
+        payload: 'foobar',
+      });
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should not select the given field if one is already selected', () => {
+      const state = {
+        currentTurn: 'white',
+        selectedField: 'foobar',
+      };
+
+      const actual = reduce(state, {
+        type: 'SELECT_FIGURE',
+        payload: 'raboof',
+      });
+
+      expect(actual).toBe(state);
+    });
+  });
+
+  describe('with SelectTargetFieldAction', () => {
+    it('should switch currentTurn and reset the selected field', () => {
+      const testTurns = [
+        {
+          currentTurn: 'white',
+          expectedTurn: 'black',
+        },
+        {
+          currentTurn: 'black',
+          expectedTurn: 'white',
+        },
+      ];
+
+      testTurns.forEach((testTurn) => {
+        const { currentTurn, expectedTurn } = testTurn;
+
+        const state = {
+          currentTurn,
+          selectedField: 'foobar',
+        };
+
+        const expected = {
+          currentTurn: expectedTurn,
+          selectedField: null,
+        };
+
+        const actual = reduce(state, {
+          type: 'SELECT_TARGET_FIELD',
+          payload: 'herpDerp',
+        });
+
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    it('should not change the state if no field is selected', () => {
+      const states = [
+        { currentTurn: 'white' },
+        {
+          currentTurn: 'black',
+          selectedField: null,
+        },
+      ];
+
+      states.forEach((state) => {
+        const actual = reduce(state, {
+          type: 'SELECT_TARGET_FIELD',
+          payload: 'foobar',
+        });
+
+        expect(actual).toBe(state);
+      });
+    });
   });
 });
