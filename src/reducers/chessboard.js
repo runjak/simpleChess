53ef485b2ad09fc35b1b202c42bd350e1d30f4d9
Jakob Runge <sicarius@g4t3.de>
2017-10-26 17:41:37 +0200

    let chessboard reducer handle MOVE_FIGURE action

--- src/reducers/chessboard.js
+++ src/reducers/chessboard.js
@@ -1,18 +1,62 @@
 // @flow
 
 import type { Figure } from '../utils/figures';
+import type { ChessboardActions, MoveFigurePayload } from '../actions/chessboard';
+
+import { positionToFieldName } from '../utils/field';
 
 import defaultBoard from './default-board.json';
 
+type Fields = { [fieldName: string]: Figure };
+
 export type ChessBoard = {
-  fields: {
-    [fieldName: string]: Figure,
-  },
+  fields: Fields,
 };
+
+function moveFigure(state: ChessBoard, payload: MoveFigurePayload): ChessBoard {
+  const { fields } = state;
+  const { sourcePosition, targetPosition } = payload;
+
+  const sourceField = positionToFieldName(sourcePosition);
+  const targetField = positionToFieldName(targetPosition);
+
+  const sourceFigure = fields[sourceField];
+  const targetFigure = fields[targetField];
+
+  if (!sourceFigure) {
+    return state;
+  }
+
+  if (targetFigure) {
+    if (targetFigure.color === sourceFigure.color) {
+      return state;
+    }
+  }
+
+  const { [sourceField]: fieldToRemove, ...untouchedFields } = fields;
+
+  return {
+    fields: {
+      ...untouchedFields,
+      [targetField]: sourceFigure,
+    },
+  };
+}
 
 export const initialState = defaultBoard;
 
 // eslint-disable-next-line no-unused-vars
-export default function reduce(state: ChessBoard = defaultBoard, action: any): ChessBoard {
-  return state;
+export default function reduce(
+  state: ChessBoard = defaultBoard,
+  action: ChessboardActions,
+): ChessBoard {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'MOVE_FIGURE':
+      return moveFigure(state, payload);
+
+    default:
+      return state;
+  }
 }
--- src/reducers/chessboard.test.js
+++ src/reducers/chessboard.test.js
@@ -11,8 +11,129 @@ describe('chessboard reducer', () => {
   });
 
   it('should return the defaultBoard as state given undefined state', () => {
-    const actual = reduce();
+    const actual = reduce(undefined, {});
 
     expect(actual).toEqual(defaultBoard);
+  });
+
+  describe('with MOVE_FIGURE action', () => {
+    const action = {
+      type: 'MOVE_FIGURE',
+      payload: {
+        sourcePosition: {
+          column: 'a',
+          row: 1,
+        },
+        targetPosition: {
+          column: 'h',
+          row: 8,
+        },
+      },
+    };
+
+    const state = {
+      fields: {
+        a1: {
+          piece: 'King',
+          color: 'black',
+        },
+        b2: {
+          piece: 'Queen',
+          color: 'white',
+        },
+        c3: {
+          piece: 'Rook',
+          color: 'black',
+        },
+      },
+    };
+
+    it('should move a figure from source to target', () => {
+      const { fields: { a1, b2, c3 } } = state;
+      const expected = {
+        fields: {
+          b2,
+          c3,
+          h8: a1,
+        },
+      };
+
+      const actual = reduce(state, action);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should not change state if source and target are the same', () => {
+      const testAction = {
+        ...action,
+        payload: {
+          ...action.payload,
+          targetPosition: action.payload.sourcePosition,
+        },
+      };
+
+      const actual = reduce(state, testAction);
+
+      expect(actual).toBe(state);
+    });
+
+    it('should overwrite figures of a different color', () => {
+      const testAction = {
+        ...action,
+        payload: {
+          ...action.payload,
+          targetPosition: {
+            column: 'b',
+            row: 2,
+          },
+        },
+      };
+
+      const { fields: { a1, c3 } } = state;
+      const expected = {
+        fields: {
+          b2: a1,
+          c3,
+        },
+      };
+
+      const actual = reduce(state, testAction);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should remain unchanged instead of overwriting the same color', () => {
+      const testAction = {
+        ...action,
+        payload: {
+          ...action.payload,
+          targetPosition: {
+            column: 'c',
+            row: 3,
+          },
+        },
+      };
+
+      const actual = reduce(state, testAction);
+
+      expect(actual).toBe(state);
+    });
+
+    it('should remain unchanged when no figure is found on sourcePosition', () => {
+      const testAction = {
+        ...action,
+        payload: {
+          ...action.payload,
+          sourcePosition: {
+            column: 'd',
+            row: 4,
+          },
+        },
+      };
+
+      const actual = reduce(state, testAction);
+
+      expect(actual).toBe(state);
+    });
   });
 });
