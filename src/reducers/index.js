25f951952f8cee65cec461a6def53704925ae834
Jakob Runge <sicarius@g4t3.de>
2017-10-26 21:51:19 +0200

    add gernal State type to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,16 @@
 // $FlowFixMe - flow doesn't know redux
 import { combineReducers } from 'redux';
 
+import type { PlayerState } from './players';
+import type { ChessBoard } from './chessboard';
+
 import players from './players';
 import chessboard from './chessboard';
+
+export type State = {
+  players: PlayerState,
+  chessboard: ChessBoard,
+};
 
 export default combineReducers({
   chessboard,
