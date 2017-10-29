0b23449c13183168f46bbb8e6d31c2aa57543248
Jakob Runge <sicarius@g4t3.de>
2017-10-29 21:49:51 +0100

    add clickField to player actions

--- src/actions/player.js
+++ src/actions/player.js
@@ -1,7 +1,18 @@
 // @flow
-
 import type { Action } from './';
 import type { Position } from '../utils/field';
+import type { State } from '../reducers';
+
+import {
+  selectCurrentTurn,
+  selectSelectedField,
+} from '../selectors/players';
+import {
+  createFigureSelector,
+  selectFields,
+} from '../selectors/chessboard';
+import { positionToFieldName, isValidMove } from '../utils/field';
+import { moveFigure } from './chessboard';
 
 export type SelectFigureAction = Action<Position> & {
   type: 'SELECT_FIGURE',
@@ -24,5 +35,40 @@ export function selectTargetField(position: Position): SelectTargetFieldAction {
   return {
     type: 'SELECT_TARGET_FIELD',
     payload: position,
+  };
+}
+
+export function clickField(clickedPosition: Position) {
+  const clickedFieldName = positionToFieldName(clickedPosition);
+
+  return (dispatch: (action: Action<any>) => void, getState: () => State) => {
+    const state = getState();
+    const maybeSourcePosition = selectSelectedField(state);
+    const fields = selectFields(state);
+    const currentTurn = selectCurrentTurn(state);
+
+    if (!maybeSourcePosition) {
+      const clickedFigure = createFigureSelector(clickedPosition)(state);
+
+      if (clickedFigure && clickedFigure.color === currentTurn) {
+        dispatch(selectFigure(clickedPosition));
+      }
+
+      return;
+    }
+
+    // $FlowFixMe - flow being picky about type refinement
+    const sourcePosition: Position = maybeSourcePosition;
+
+    if (positionToFieldName(sourcePosition) === clickedFieldName) {
+      return;
+    }
+
+    if (!isValidMove(fields, positionToFieldName(sourcePosition), clickedFieldName)) {
+      return;
+    }
+
+    dispatch(selectTargetField(clickedPosition));
+    dispatch(moveFigure(sourcePosition, clickedPosition));
   };
 }
--- src/actions/player.test.js
+++ src/actions/player.test.js
@@ -1,4 +1,14 @@
-import { selectFigure, selectTargetField } from './player';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import {
+  selectFigure,
+  selectTargetField,
+  clickField,
+} from './player';
+import state from './player-test-state.json';
+
+const mockStore = configureStore([thunk]);
 
 describe('player actions', () => {
   describe('selectFigure()', () => {
@@ -24,6 +34,108 @@ describe('player actions', () => {
       const actual = selectTargetField('foobar');
 
       expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('clickField()', () => {
+    const stateA1Selected = {
+      ...state,
+      players: {
+        ...state.players,
+        selectedField: {
+          column: 'a',
+          row: 1,
+        },
+      },
+    };
+    const clickFieldAction = clickField({
+      column: 'a',
+      row: 1,
+    });
+
+    it('should not dispatch if the clicked field is already selected', () => {
+      const store = mockStore(stateA1Selected);
+
+      store.dispatch(clickFieldAction);
+      const actions = store.getActions();
+
+      expect(actions).toEqual([]);
+    });
+
+    it('should not dispatch if trying to select opponents figure', () => {
+      const store = mockStore(state);
+
+      store.dispatch(clickField({
+        column: 'b',
+        row: 2,
+      }));
+      const actions = store.getActions();
+
+      expect(actions).toEqual([]);
+    });
+
+    it('should dispatch a SELECT_FIGURE action if no figure is selected', () => {
+      const store = mockStore(state);
+
+      const expected = [{
+        type: 'SELECT_FIGURE',
+        payload: {
+          column: 'a',
+          row: 1,
+        },
+      }];
+
+      store.dispatch(clickFieldAction);
+      const actions = store.getActions();
+
+      expect(actions).toEqual(expected);
+    });
+
+    it('should not dispatch if the target move is not valid', () => {
+      const store = mockStore(stateA1Selected);
+
+      store.dispatch(clickField({
+        column: 'c',
+        row: 3,
+      }));
+      const actions = store.getActions();
+
+      expect(actions).toEqual([]);
+    });
+
+    it('should dispatch SELECT_TARGET_FIELD and MOVE_FIGURE on valid mvoe', () => {
+      const store = mockStore(stateA1Selected);
+
+      const expected = [
+        {
+          type: 'SELECT_TARGET_FIELD',
+          payload: {
+            column: 'b',
+            row: 2,
+          },
+        },
+        {
+          type: 'MOVE_FIGURE',
+          payload: {
+            sourcePosition: {
+              column: 'a',
+              row: 1,
+            },
+            targetPosition: {
+              column: 'b',
+              row: 2,
+            },
+          },
+        },
+      ];
+
+      store.dispatch(clickField({
+        column: 'b',
+        row: 2,
+      }));
+      const actions = store.getActions();
+
+      expect(actions).toEqual(expected);
     });
   });
 });
